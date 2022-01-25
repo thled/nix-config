b7e49247c0554d3902a4bdc419e3b2ec250f564c
Thomas Le Duc <dev@tleduc.de>
2022-01-25 11:47:49 +0100

    Add hostname and timezone

--- nix-config/system/common.nix
+++ nix-config/system/common.nix
@@ -1,4 +1,6 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
+  time.timeZone = "Europe/Berlin";
+  networking.hostName = "NB61";
   environment.variables = {
     XDG_CONFIG_HOME = "/etc/config";
   };
