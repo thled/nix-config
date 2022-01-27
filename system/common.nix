53b2adbdcbaf18e476065821ea70d2caba795cbc
Thomas Le Duc <dev@tleduc.de>
2022-01-27 08:45:17 +0100

    Add SourceCodePro font

--- system/common.nix
+++ system/common.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }: {
   time.timeZone = "Europe/Berlin";
   networking.hostName = "NB61";
+
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+  ];
+
   environment.variables = {
     XDG_CONFIG_HOME = "/etc/config";
   };
