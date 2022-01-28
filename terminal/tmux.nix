c480689082c0f4de4ee0d5fb7556bad12cf6bd44
Thomas Le Duc <dev@tleduc.de>
2022-01-28 16:31:29 +0100

    Update tmux config

--- terminal/tmux.nix
+++ terminal/tmux.nix
@@ -2,12 +2,24 @@
   programs.tmux = {
     enable = true;
     baseIndex = 1;
-    escapeTime = 1;
+    customPaneNavigationAndResize = true;
+    escapeTime = 0;
     historyLimit = 10000;
     keyMode = "vi";
     newSession = true;
     shortcut = "a";
     terminal = "tmux-256color";
+
+    extraConfig = ''
+      bind-key C-a send-prefix
+      bind-key Escape copy-mode
+      bind-key \\ split-window -h -c '#{pane_current_path}'
+      bind-key - split-window -v -c '#{pane_current_path}'
+      set-option -g status-style bg=black,fg=white
+      set-option -g window-status-style bg=black,fg=colour8
+      set-option -g window-status-current-style bg=black,fg=white,bold
+      set-option -g status-right '%m/%d %H:%M'
+    '';
   };
 }
 
