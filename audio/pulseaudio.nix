56ec66b0384d6322455de060b78bbbda322a5a07
Thomas Le Duc <dev@tleduc.de>
2022-01-28 07:31:54 +0100

    Add audio control

--- audio/pulseaudio.nix
+++ audio/pulseaudio.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }: {
   hardware.pulseaudio.enable = true;
   nixpkgs.config.pulseaudio = true;
+
+  systemd.tmpfiles.rules = [
+    "d /etc/config/pulse 0755 thled root -"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
 }
 
