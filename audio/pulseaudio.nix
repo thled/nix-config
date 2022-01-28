ad0081d97f9e94781964e13286c34b317ce3b138
Thomas Le Duc <dev@tleduc.de>
2022-01-28 07:39:44 +0100

    Fix pavucontrol config

--- audio/pulseaudio.nix
+++ audio/pulseaudio.nix
@@ -2,12 +2,13 @@
   hardware.pulseaudio.enable = true;
   nixpkgs.config.pulseaudio = true;
 
-  systemd.tmpfiles.rules = [
-    "d /etc/config/pulse 0755 thled root -"
-  ];
-
   environment.systemPackages = with pkgs; [
     pavucontrol
+  ];
+
+  systemd.tmpfiles.rules = [
+    "d /etc/config/pulse 0755 thled root -"
+    "f /etc/config/pavucontrol.ini 0755 thled root -"
   ];
 }
 
