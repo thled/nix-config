e5fc7c5825f4145f8d09d019a7cc8ac1bd6e6e91
Thomas Le Duc <dev@tleduc.de>
2022-02-10 07:31:48 +0100

    Add Vim general settings

--- editor/vim.nix
+++ editor/vim.nix
@@ -4,6 +4,41 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    withNodeJs = false;
+    withPython3 = true;
+    withRuby = false;
+    configure = {
+      customRC = ''
+        """""""""""""""
+        """ general """
+        """""""""""""""
+
+        " leader key
+        let mapleader = "\<Space>"
+
+        " allow unsaved buffer switch
+        set hidden
+
+        " line numbers
+        set number relativenumber
+
+        " search
+        set ignorecase smartcase
+
+        " indentation
+        set tabstop=4 softtabstop=4 shiftwidth=4 expandtab smartindent
+
+        " incremental substitute
+        set inccommand=nosplit
+
+        " ruler
+        set colorcolumn=120
+        highlight ColorColumn ctermbg=238 guibg=lightgrey
+
+        " keymap for no highlighting
+        nnoremap <nowait><silent> <C-C> :nohlsearch<CR>
+      '';
+    };
   };
 }
 
