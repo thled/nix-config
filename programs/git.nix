98d350185fc7c4334348e18090b025427c1f4d0f
Thomas Le Duc <dev@tleduc.de>
2022-02-25 17:00:48 +0100

    Separate git configs

--- programs/git.nix
+++ programs/git.nix
@@ -8,7 +8,19 @@
       difftool.meld.cmd = "meld \"$LOCAL\" \"$REMOTE\"";
       merge.tool = "meld";
       mergetool.meld.cmd = "meld \"$LOCAL\" \"$MERGED\" \"$REMOTE\" --output \"$MERGED\"";
+      user.name = "Thomas Le Duc";
+      includeIf."gitdir:~/projects/".path = "/etc/gitconfig.private";
+      includeIf."gitdir:/etc/nixos/nix-config/".path = "/etc/gitconfig.private";
+      includeIf."gitdir:~/work/".path = "/etc/gitconfig.work";
     };
   };
+
+  environment.etc."gitconfig.private".text =
+    "[user]
+    email = dev@tleduc.de";
+
+  environment.etc."gitconfig.work".text =
+    "[user]
+    email = leduc@publicplan.de";
 }
 
