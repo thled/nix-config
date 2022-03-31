ed2af258fda09c5d6dc24b0602a2273056f586df
Thomas Le Duc <dev@tleduc.de>
2022-03-31 11:41:42 +0200

    Add difftastic

--- programs/git.nix
+++ programs/git.nix
@@ -2,12 +2,15 @@
   programs.git = {
     enable = true;
     config = {
+      alias.dt = "difftool";
       init.defaultBranch = "main";
-      diff.tool = "meld";
+      diff.tool = "difftastic";
       difftool.prompt = "false";
       difftool.meld.cmd = "meld \"$LOCAL\" \"$REMOTE\"";
+      difftool.difftastic.cmd = "difftastic \"$LOCAL\" \"$REMOTE\"";
       merge.tool = "meld";
       mergetool.meld.cmd = "meld \"$LOCAL\" \"$MERGED\" \"$REMOTE\" --output \"$MERGED\"";
+      pager.difftool = "true";
       user.name = "Thomas Le Duc";
       includeIf."gitdir:~/projects/".path = "/etc/gitconfig.private";
       includeIf."gitdir:/etc/nixos/nix-config/".path = "/etc/gitconfig.private";
--- programs/utils.nix
+++ programs/utils.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     bat
+    difftastic
     direnv
     dunst
     exa
