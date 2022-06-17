4537c35055935107c76bb8215d9a4e99b2b28ad3
Thomas Le Duc <dev@tleduc.de>
2022-06-17 07:28:36 +0200

    Use USB-C for monitor

--- wm/xmonad.hs
+++ wm/xmonad.hs
@@ -28,9 +28,8 @@ myConfig = def
         `additionalKeysP`
         [ ("M-x", spawn "slock")
         , ("M-s", spawn "import ~/screenshots/$(date +\"%Y-%m-%d_%H-%M-%S\").png")
-        , ("M-C-1", spawn "xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off")
-        , ("M-C-2", spawn "xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --primary --mode 3840x1080 --pos 0x0 --rotate normal")
-        , ("M-C-3", spawn "xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal")
+        , ("M-C-1", spawn "xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off")
+        , ("M-C-2", spawn "xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off")
         ]
 
 myLayouts = layoutMulti ||| layoutCenter ||| layoutTall
