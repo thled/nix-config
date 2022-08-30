2d5dfbd658bcc74fff95387e6b406a3050ceca06
Thomas Le Duc <dev@tleduc.de>
2022-08-30 20:43:24 +0200

    Add catppuccin theme to zellij

--- terminal/zellij.nix
+++ terminal/zellij.nix
@@ -1,5 +1,11 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ zellij ];
+{ config, pkgs, ... }:
+let
+  unstable = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/nixos-unstable)
+    { config = config.nixpkgs.config; };
+in
+{
+  environment.systemPackages = with pkgs; [ unstable.zellij ];
   environment.etc."config/zellij/config.yaml".source = ./zellij.yaml;
+  environment.etc."config/zellij/themes/catppuccin.yaml".source = ./catppuccin.yaml;
 }
-
