0c4661a0f63d21bd3593947936d48bc8b7749f16
Thomas Le Duc <dev@tleduc.de>
2022-09-19 07:53:45 +0200

    Use extern SMTP server to send inbox mail

--- network/mail.nix
+++ network/mail.nix
@@ -1,3 +1,15 @@
 { pkgs, ... }: {
-  services.postfix.enable = true;
+  programs.msmtp = {
+    enable = true;
+    accounts = {
+      default = {
+        auth = true;
+        tls = true;
+        from = "thled_inbox@web.de";
+        host = "smtp.web.de";
+        user = "thled_inbox";
+        passwordeval = "cat ~/.inbox_pass";
+      };
+    };
+  };
 }
