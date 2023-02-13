b2d51969393519b38cce4e4598d9b91003bc3878
Thomas Le Duc <dev@tleduc.de>
2023-02-13 16:09:53 +0100

    Remove Forti VPN

--- network/networkmanager.nix
+++ network/networkmanager.nix
@@ -1,14 +1,6 @@
 { pkgs, ... }: {
   networking.networkmanager = {
     enable = true;
-    plugins = [
-      pkgs.networkmanager-fortisslvpn
-    ];
-  };
-
-  environment.etc."NetworkManager/system-connections/office.nmconnection" = {
-    source = ./office.nmconnection;
-    mode = "0600";
   };
 }
 
