11fd731bc2803ab986b487d774d6468b9ee36ad3
Thomas Le Duc <dev@tleduc.de>
2023-02-17 10:47:38 +0100

    Change Docker network bridge IP

--- virtualisation/docker.nix
+++ virtualisation/docker.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }: {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      bip = "10.200.0.1/24";
+      "default-address-pools" = [
+        {
+          "base" = "10.200.0.0/16";
+          "size" = 24;
+        }
+      ];
+    };
+  };
+
   boot.kernel.sysctl = {
     "vm.max_map_count" = 262144;
   };
