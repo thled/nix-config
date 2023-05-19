739c2eb74801a8efca6602322bad03aa9407fbd2
Thomas Le Duc <dev@tleduc.de>
2023-05-19 08:34:18 +0200

    Update Syncthing config

--- programs/syncthing.nix
+++ programs/syncthing.nix
@@ -6,17 +6,19 @@
     configDir = "/home/thled/sync/.config/syncthing";
     overrideDevices = true;
     overrideFolders = true;
-    devices = {
-      "pixel4a" = { id = "A4BICQY-5SI372K-YGFDIMG-VI2EEET-0Y72ZER-YD6WGCG-V30NUKI-Q7R3YAN"; };
-    };
-    folders = {
-      "Sync" = {
-        path = "/home/thled/sync";
-        devices = [ "pixel4a" ];
+    settings = {
+      devices = {
+        "pixel4a" = { id = "A4BICQY-5SI372K-YGFDIMG-VI2EEET-0Y72ZER-YD6WGCG-V30NUKI-Q7R3YAN"; };
       };
-      "Obsidian" = {
-        path = "/home/thled/projects/references";
-        devices = [ "pixel4a" ];
+      folders = {
+        "Sync" = {
+          path = "/home/thled/sync";
+          devices = [ "pixel4a" ];
+        };
+        "Obsidian" = {
+          path = "/home/thled/projects/references";
+          devices = [ "pixel4a" ];
+        };
       };
     };
   };
