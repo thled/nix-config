fd4907d34daff13577adbf653521c5360c9da088
Thomas Le Duc <dev@tleduc.de>
2023-05-26 14:42:46 +0200

    [syncthing] Revert wrapping conf in `settings`

--- programs/syncthing.nix
+++ programs/syncthing.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ ... }: {
   services.syncthing = {
     enable = true;
     user = "thled";
@@ -6,19 +6,17 @@
     configDir = "/home/thled/sync/.config/syncthing";
     overrideDevices = true;
     overrideFolders = true;
-    settings = {
-      devices = {
-        "pixel4a" = { id = "A4BICQY-5SI372K-YGFDIMG-VI2EEET-0Y72ZER-YD6WGCG-V30NUKI-Q7R3YAN"; };
+    devices = {
+      "pixel4a" = { id = "A4BICQY-5SI372K-YGFDIMG-VI2EEET-0Y72ZER-YD6WGCG-V30NUKI-Q7R3YAN"; };
+    };
+    folders = {
+      "Sync" = {
+        path = "/home/thled/sync";
+        devices = [ "pixel4a" ];
       };
-      folders = {
-        "Sync" = {
-          path = "/home/thled/sync";
-          devices = [ "pixel4a" ];
-        };
-        "Obsidian" = {
-          path = "/home/thled/projects/references";
-          devices = [ "pixel4a" ];
-        };
+      "Obsidian" = {
+        path = "/home/thled/projects/references";
+        devices = [ "pixel4a" ];
       };
     };
   };
