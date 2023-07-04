db11b68f2475ef8297bedbbebdbfa549a94d6ae1
Thomas Le Duc <dev@tleduc.de>
2023-07-04 08:12:06 +0200

    [zellij] Make script for zen mode more clever

--- terminal/zellij.nix
+++ terminal/zellij.nix
@@ -1,5 +1,15 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ zellij ];
+{ pkgs, ... }: 
+let
+  myZellij = pkgs.zellij.overrideAttrs (final: previous: {
+    src = pkgs.fetchFromGitHub {
+      owner = "thled";
+      repo = "zellij";
+      rev = "query-pane-names";
+      sha256 = "sha256-LD+1cKd7DCkergHhmoXI9+PtRdPRRCnKvpxM/GHSy9I=";
+    };
+  });
+in {
+  environment.systemPackages = [ myZellij ];
   environment.etc."config/zellij/config.kdl".source = ./zellij.kdl;
   environment.etc."config/zellij/zellij_zen.sh".source = ./zellij_zen.sh;
 }
--- terminal/zellij_zen.sh
+++ terminal/zellij_zen.sh
@@ -1,30 +1,24 @@
 #!/usr/bin/env sh
 
-exit=false
+pane_names=$(zellij action query-pane-names)
+pane_count=$(echo "$pane_names" | wc -l)
 
-for arg in "$@"
-do
-  if [ "$arg" == "--exit" ]; then
-    exit=true
-    set -- "${@//--exit/}"
-    break
-  fi
-done
+if [ "$pane_count" -eq 2 ]; then
+  zellij action new-pane --direction right
+  zellij action new-pane --direction right
+  zellij action move-focus left
+  zellij action move-focus left
+  zellij action new-pane --direction right
+  zellij action move-pane left
+  zellij action move-focus right
+  zellij action move-focus right
+  zellij action close-pane
+fi
 
-if [ "$exit" = true ]; then
+if [ "$pane_count" -eq 4 ]; then
   zellij action move-focus left
   zellij action close-pane
   zellij action move-focus right
   zellij action close-pane
   exit
 fi
-
-zellij action new-pane --direction right
-zellij action new-pane --direction right
-zellij action move-focus left
-zellij action move-focus left
-zellij action new-pane --direction right
-zellij action move-pane left
-zellij action move-focus right
-zellij action move-focus right
-zellij action close-pane
