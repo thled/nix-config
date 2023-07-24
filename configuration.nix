bd8e5a0d3965182b1529de562f4110aceab2d92e
Thomas Le Duc <dev@tleduc.de>
2023-07-24 09:29:29 +0200

    [syncthing] Use fix from master

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,9 @@
-{ ... }:
+{ master, ... }:
 {
+  disabledModules = [ "services/networking/syncthing.nix" ];
   imports = [
     ./hardware-configuration.nix
+    (master + "/nixos/modules/services/networking/syncthing.nix")
     ./modules
   ];
 
--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # home-manager = {
-    #   url = "github:nix-community/home-manager/release-23.05";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    master.url = "github:NixOS/nixpkgs/master";
 
     helix.url = "github:helix-editor/helix/23.05";
   };
