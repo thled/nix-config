26acc6d382be2405657142a5cb5e0986c2aa5c2f
Thomas Le Duc <dev@tleduc.de>
2023-08-08 18:45:54 +0200

    [audio] Switch to PipeWire

--- modules/audio/pulseaudio.nix
+++ modules/audio/pulseaudio.nix
@@ -1,20 +1,31 @@
 { pkgs, ... }: {
-  sound.enable = true;
-  hardware = {
-    pulseaudio = {
-      enable = true;
-      extraConfig = ''
-        load-module module-null-sink sink_name=Source
-        load-module module-virtual-source source_name=VirtualMic master=Source.monitor
-      '';
-    };
-    bluetooth.enable = true;
+  hardware.pulseaudio.enable = false;
+  hardware.bluetooth.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    wireplumber.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
   };
-  nixpkgs.config.pulseaudio = true;
+  
+  # sound.enable = true;
+  # hardware = {
+  #   pulseaudio = {
+  #     enable = true;
+  #     extraConfig = ''
+  #       load-module module-null-sink sink_name=Source
+  #       load-module module-virtual-source source_name=VirtualMic master=Source.monitor
+  #     '';
+  #   };
+  #   bluetooth.enable = true;
+  # };
+  # nixpkgs.config.pulseaudio = true;
 
-  systemd.tmpfiles.rules = [
-    "d /etc/config/pulse 0755 thled root -"
-  ];
+  # systemd.tmpfiles.rules = [
+  #   "d /etc/config/pulse 0755 thled root -"
+  # ];
 
   environment.systemPackages = [ pkgs.pavucontrol ];
 }
