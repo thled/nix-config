42f618434c4d7017b39a4ae163f19d267128ac0b
Thomas Le Duc <dev@tleduc.de>
2023-08-10 10:53:09 +0200

    [terminal] remove zen script

--- modules/terminal/zellij.nix
+++ modules/terminal/zellij.nix
@@ -1,15 +1,5 @@
 { pkgs, ... }: 
-let
-  myZellij = pkgs.zellij.overrideAttrs (final: previous: {
-    src = pkgs.fetchFromGitHub {
-      owner = "thled";
-      repo = "zellij";
-      rev = "query-pane-names";
-      sha256 = "sha256-LD+1cKd7DCkergHhmoXI9+PtRdPRRCnKvpxM/GHSy9I=";
-    };
-  });
-in {
-  environment.systemPackages = [ myZellij ];
+{
+  environment.systemPackages = [ pkgs.zellij ];
   environment.etc."config/zellij/config.kdl".source = ./zellij.kdl;
-  environment.etc."config/zellij/zellij_zen.sh".source = ./zellij_zen.sh;
 }
