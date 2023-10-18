0b55d6aba555cbe75758ea4f944466a854b6ab98
Thomas Le Duc <dev@tleduc.de>
2023-10-18 18:08:42 +0200

    [terminal] Use direnv module

--- modules/terminal/direnv.nix
+++ modules/terminal/direnv.nix
@@ -1,17 +1,3 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ direnv nix-direnv ];
-
-  nix.settings = {
-    keep-outputs = true;
-    keep-derivations = true;
-  };
-  environment.pathsToLink = [
-    "/share/nix-direnv"
-  ];
-
-  nixpkgs.overlays = [
-    (self: super: { nix-direnv = super.nix-direnv.override { enableFlakes = true; }; } )
-  ];
-
-  environment.etc."config/direnv/direnvrc".source = ./direnvrc;
+{ ... }: {
+  programs.direnv.enable = true;
 }
