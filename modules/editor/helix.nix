eee2533749bc5cbc18d1d7f1400081a496569b3e
Thomas Le Duc <dev@tleduc.de>
2023-11-03 17:23:55 +0100

    [helix] Use Intelephense and Phpactor LSPs

--- modules/editor/helix.nix
+++ modules/editor/helix.nix
@@ -1,5 +1,5 @@
 { pkgs, helix, ... }: {
   environment.systemPackages = [ helix.packages."${pkgs.system}".helix ];
   environment.etc."config/helix/config.toml".source = ./helix.toml;
-  # environment.etc."config/helix/languages.toml".source = ./helix_languages.toml;
+  environment.etc."config/helix/languages.toml".source = ./helix_languages.toml;
 }
