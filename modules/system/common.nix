39b061c6356392f173f323a86148f5e582183220
Thomas Le Duc <dev@tleduc.de>
2023-11-14 17:32:50 +0100

    [system] Use Noto for emojis

--- modules/system/common.nix
+++ modules/system/common.nix
@@ -11,6 +11,12 @@
     packages = with pkgs; [
       cascadia-code
       noto-fonts
+      noto-fonts-lgc-plus
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      noto-fonts-emoji-blob-bin
+      noto-fonts-monochrome-emoji
     ];
     fontconfig = {
       defaultFonts = {
