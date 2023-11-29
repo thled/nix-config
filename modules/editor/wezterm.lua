18d68ed7db0bb517655b9feee6fcd1c4e4dec421
Thomas Le Duc <dev@tleduc.de>
2023-11-29 07:44:58 +0100

    [wezterm] keybind session selector

--- modules/editor/wezterm.lua
+++ modules/editor/wezterm.lua
@@ -1,14 +1,22 @@
 local wezterm = require 'wezterm'
 local config = {}
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+if wezterm.config_builder then config = wezterm.config_builder() end
 
 config.font = wezterm.font 'Cascadia Code'
 config.font_size = 14.0
 
 config.color_scheme = 'Tomorrow Night'
-
 config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+config.scrollback_lines = 10000
+
+config.keys = {
+  {
+    key = 's',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' },
+  },
+}
 
 return config
