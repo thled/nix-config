d354c9707ba7ae1f4653b4200b1896f3fe91f58f
Thomas Le Duc <dev@tleduc.de>
2023-12-06 08:04:19 +0100

    Revert "[clipcat] deactivate because of bug https://github.com/xrelkd/clipcat/issues/32"
    This reverts commit c3ba36e848d8de2c689f7659d74c5ff1de2ed6f8.

--- modules/programs/clipcat/clipcat.nix
+++ modules/programs/clipcat/clipcat.nix
@@ -1,22 +1,22 @@
 { pkgs, ... }: {
-  # environment.systemPackages = with pkgs; [ clipcat dmenu ];
+  environment.systemPackages = with pkgs; [ clipcat dmenu ];
 
   environment.etc."config/clipcat/clipcatd.toml".source = ./clipcatd.toml;
   environment.etc."config/clipcat/clipcatctl.toml".source = ./clipcatctl.toml;
   environment.etc."config/clipcat/clipcat-menu.toml".source = ./clipcat-menu.toml;
 
-  # systemd.services.clipcatd = {
-  #   description = "Clipcat Daemon";
-  #   after = [ "display-manager.service" ];
-  #   requires = [ "display-manager.service" ];
-  #   wantedBy = [ "multi-user.target" ];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.clipcat}/bin/clipcatd --no-daemon";
-  #     Restart = "always";
-  #     User = "thled";
-  #     Group = "users";
-  #     Environment = "XDG_CONFIG_HOME=/etc/config DISPLAY=:0";
-  #   };
-  # };
+  systemd.services.clipcatd = {
+    description = "Clipcat Daemon";
+    after = [ "display-manager.service" ];
+    requires = [ "display-manager.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.clipcat}/bin/clipcatd --no-daemon";
+      Restart = "always";
+      User = "thled";
+      Group = "users";
+      Environment = "XDG_CONFIG_HOME=/etc/config DISPLAY=:0";
+    };
+  };
 }
 
