640c031d9374f5df7bd062f001dc85fcc44aa005
Thomas Le Duc <dev@tleduc.de>
2023-12-21 08:43:25 +0100

    [clipcat] deactivate again

--- modules/default.nix
+++ modules/default.nix
@@ -20,7 +20,7 @@
     ./virtualisation/virt_manager.nix
     ./network/vpn.nix
     ./programs/utils.nix
-    ./programs/clipcat/clipcat.nix
+    # ./programs/clipcat/clipcat.nix
     ./programs/git.nix
     ./programs/kubernetes.nix
     ./programs/ripgrep.nix
