8f2810d076bc4447a5bde37ca4a13dbf4f613238
Thomas Le Duc <dev@tleduc.de>
2024-03-18 18:19:05 +0100

    [hyprland] Add NVIDIA GPU

--- modules/wm/hyprland.nix
+++ modules/wm/hyprland.nix
@@ -1,29 +1,55 @@
-{ pkgs, ... }: {
+{ pkgs
+, lib
+, config
+, ... }: {
+  services.xserver.videoDrivers = ["nvidia"];
+
   hardware = {
     nvidia = {
-      # powerManagement.enable = true;
       modesetting.enable = true;
+      nvidiaSettings = true;
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
       prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
+        # Option A: Offload Mode
+        # offload = {
+        #   enable = lib.mkOverride 990 true;
+        #   enableOffloadCmd = lib.mkIf config.hardware.nvidia.prime.offload.enable true;
+        # };
+
+        # Option B: Sync Mode
+        sync.enable = true;
+
+        intelBusId = lib.mkDefault "PCI:0:2:0";
+        nvidiaBusId = lib.mkDefault "PCI:1:0:0";
       };
+      powerManagement.enable = false;
     };
-    opengl.enable = true;
+    opengl = {
+        driSupport = true;
+        driSupport32Bit = true;
+        enable = true;
+        extraPackages = with pkgs; [nvidia-vaapi-driver intel-media-driver];
+        extraPackages32 = with pkgs.pkgsi686Linux; [nvidia-vaapi-driver intel-media-driver];
+    };
   };
 
-  programs.hyprland.enable = true;
-  # hint electron apps to use wayland
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
   environment = {
     etc."config/hypr/hyprland.conf".source = ./hyprland.conf;
     etc."config/hypr/hypridle.conf".source = ./hypridle.conf;
     etc."config/waybar/config".source = ./waybar.jsonc;
     etc."config/waybar/style.css".source = ./waybar.css;
+    sessionVariables = {
+      # hint electron apps to use wayland
+      NIXOS_OZONE_WL = "1";
+      # if cursor invisible
+      # WLR_NO_HARDWARE_CURSORS = "1";
+    };
     systemPackages = with pkgs; [ 
       fuzzel
       hypridle
@@ -32,9 +58,25 @@
       wl-clipboard
       wlr-randr
       xdg-desktop-portal-gtk
+
+      (pkgs.writeShellScriptBin "nvidia-offload" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec "$0"
+      '')
     ];
   };
 
   security.pam.services.waylock = {};
+
+  # screen sharing
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    wlr.enable = true;
+  };
 }
 
