0632d3d2f1685e8c4651c774f64f0ff707568f9e
Thomas Le Duc <dev@tleduc.de>
2024-04-29 06:53:54 +0200

    [fish] Remove greeting

--- modules/terminal/config.fish
+++ modules/terminal/config.fish
@@ -1,14 +1,14 @@
-function starship_transient_prompt_func
-    printf (set_color 949494)"$(date '+%H:%M') \$ "
-end
-enable_transience
-
-fish_config theme choose fish\ default
-# set fish_color_autosuggestion 555
-
 direnv hook fish | source
 
 if status is-interactive
+    function starship_transient_prompt_func
+        printf (set_color 949494)"$(date '+%H:%M') \$ "
+    end
+    enable_transience
+
+    set -U fish_greeting
+    fish_config theme choose fish\ default
+
     atuin init fish | source
     zoxide init fish | source
 end
