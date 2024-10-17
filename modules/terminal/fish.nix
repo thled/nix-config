bc67b0df1bbf92ed6a178c2d7099aec51b86d11a
Thomas Le Duc <dev@tleduc.de>
2024-10-17 18:09:16 +0200

    [fish] change `gl` alias

--- modules/terminal/fish.nix
+++ modules/terminal/fish.nix
@@ -10,7 +10,7 @@ _: {
       gdt = "git difftool";
       gf  = "git fetch";
       gg  = "git gui";
-      gl  = "git log";
+      gl  = "git log --pretty='tformat:%C(always,yellow)%h%C(always,reset) %C(always,green)%ar%C(always,reset) %C(always,bold blue)%an%C(always,reset) %C(always,red)%d%C(always,reset) %s'";
       gm  = "git merge";
       gp  = "git push";
       gs  = "git status";
