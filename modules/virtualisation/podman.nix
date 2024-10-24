f4b25cabcb3ca1b903c8cdb0449cc83f8d2eac5e
Thomas Le Duc <dev@tleduc.de>
2024-10-24 09:49:52 +0200

    [virtualisation] change from docker to podman

---
+++ modules/virtualisation/podman.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    autoPrune = {
+      enable = true;
+      dates = "weekly";
+      flags = [
+        "--filter=until=24h"
+      ];
+    };
+    defaultNetwork.settings.dns_enabled = true;
+  };
+
+  environment.systemPackages = with pkgs; [ podman-compose ];
+}
