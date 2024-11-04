80d4d1ef97101bd7e47a026619acfc2bab92e2fd
Thomas Le Duc <dev@tleduc.de>
2024-11-04 11:49:27 +0100

    [smartcat] update

--- modules/programs/smartcat.nix
+++ modules/programs/smartcat.nix
@@ -2,26 +2,25 @@
 , rustPlatform
 , fetchFromGitHub
 , pkg-config
-, xorg
 , openssl
 }:
 
 rustPlatform.buildRustPackage {
   pname = "smartcat";
-  version = "2.0.0";
+  version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "efugier";
     repo = "smartcat";
-    rev = "eedc6bc16764d3cd9ccb37d7ac0323039ccba724";
-    hash = "sha256-1t7t/O157VpnbFkd5RyYVV1QFycMSxd3NMWUmCu3xuc=";
+    rev = "a3543ccfdd9649d7bb3debb7b2a58906abb617cc";
+    hash = "sha256-QoMBQ/Xjh/xbsE9HthUKwm5v2tiN1tC2u6I/aOeO6ws=";
   };
 
-  cargoHash = "sha256-6g1KcMYBqOPTPJxo76vHBVOFSZWYlACz1tZE8VAR/Rk=";
+  cargoHash = "sha256-SAv2tgo5jBSsVhLM2FR5S9Sg0yZBghSKKSV9hhUCvCk=";
 
   nativeBuildInputs = [ pkg-config ];
 
-  buildInputs = [ xorg.libX11 openssl ];
+  buildInputs = [ openssl ];
 
   meta = with lib; {
     description = "Integrating language models in the Unix commands ecosystem through text streams.";
