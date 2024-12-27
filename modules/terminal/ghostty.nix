f2626f6a1578b0cdac653ae78d1712ac22435be9
Thomas Le Duc <dev@tleduc.de>
2024-12-27 10:50:25 +0100

    [ghostty] use as default term

--- modules/terminal/ghostty.nix
+++ modules/terminal/ghostty.nix
@@ -1,4 +1,4 @@
 { pkgs, ghostty, ... }: {
   environment.systemPackages = [ ghostty.packages."${pkgs.system}".default ];
-  # environment.etc."config/ghostty/config".source = ./ghosttyrc;
+  environment.etc."config/ghostty/config".source = ./ghosttyrc;
 }
