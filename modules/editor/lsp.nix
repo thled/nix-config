c36bae10af6c10477cb86b13cbd2b4ca5501846f
Thomas Le Duc <dev@tleduc.de>
2025-10-20 08:30:21 +0200

    [nixos] update

--- modules/editor/lsp.nix
+++ modules/editor/lsp.nix
@@ -2,7 +2,7 @@
   environment.systemPackages = with pkgs; [
     nodePackages.bash-language-server # bash
     nodePackages.vscode-langservers-extracted # css/html/json
-    nodePackages.dockerfile-language-server # docker
+    dockerfile-language-server # docker
     go # go
     gopls # go
     jdt-language-server # java
