60f77babdb99fe081c1b4b060533a165b23b2119
Thomas Le Duc <dev@tleduc.de>
2026-01-29 11:21:09 +0100

    [helix] create gruvbox-minimal theme

--- modules/editor/helix.nix
+++ modules/editor/helix.nix
@@ -8,6 +8,12 @@
     etc = {
       "config/helix/config.toml".source = ./helix.toml;
       "config/helix/languages.toml".source = ./helix_languages.toml;
+      "config/helix/themes/gruvbox_minimal.toml".source = ./gruvbox_minimal.toml;
+      "config/helix/runtime/queries/rust/highlights.scm".source = ./helix_rust_highlights.scm;
+      "config/helix/runtime/queries/php/highlights.scm".source = ./helix_php_highlights.scm;
+      "config/helix/runtime/queries/php-only/highlights.scm".source = ./helix_php-only_highlights.scm;
+      "config/helix/runtime/queries/typescript/highlights.scm".source = ./helix_ts_highlights.scm;
+      "config/helix/runtime/queries/tsx/highlights.scm".source = ./helix_tsx_highlights.scm;
     };
   };
 }
---
+++ modules/editor/helix_ts_highlights.scm
@@ -0,0 +1,23 @@
+; inherits: ecma,_typescript
+
+(class
+  name: (type_identifier) @type.definition)
+(class_declaration
+  name: (type_identifier) @type.definition)
+(interface_declaration
+  name: (type_identifier) @type.definition)
+(type_alias_declaration
+  name: (type_identifier) @type.definition)
+(enum_declaration
+  name: (identifier) @type.definition)
+(abstract_class_declaration
+  name: (type_identifier) @type.definition)
+
+(call_expression
+  function: (identifier) @function.call)
+(call_expression
+  function: (member_expression
+    property: (property_identifier) @function.method.call))
+(call_expression
+  function: (member_expression
+    property: (private_property_identifier) @function.method.private.call))
