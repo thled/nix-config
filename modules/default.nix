79bdde8d33fa649697497acd4b24d3cde1fa0174
Thomas Le Duc <dev@tleduc.de>
2026-02-13 13:29:09 +0100

    [virt] add virt-manager

--- modules/default.nix
+++ modules/default.nix
@@ -18,7 +18,7 @@
     ./editor/helix.nix
     ./virtualisation/docker.nix
     # ./virtualisation/podman.nix
-    # ./virtualisation/virt_manager.nix
+    ./virtualisation/virt_manager.nix
     ./network/vpn.nix
     ./programs/utils.nix
     ./programs/git.nix
