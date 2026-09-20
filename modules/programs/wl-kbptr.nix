a219519896bc0971a87e7d5cad9171a79afacde3
Thomas Le Duc <dev@tleduc.de>
2026-09-20 15:14:44 +0200

    [wlkbptr] make labels bold

--- modules/programs/wl-kbptr.nix
+++ modules/programs/wl-kbptr.nix
@@ -1,12 +1,20 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  wlKbptrBold = pkgs.wl-kbptr.overrideAttrs (old: {
+    postPatch = (old.postPatch or "") + ''
+      substituteInPlace src/mode_floating.c \
+        --replace-fail CAIRO_FONT_WEIGHT_NORMAL CAIRO_FONT_WEIGHT_BOLD
+    '';
+  });
+in {
   environment = {
-    systemPackages = [pkgs.wl-kbptr];
+    systemPackages = [wlKbptrBold];
     etc."config/wl-kbptr/config".text = ''
       [general]
       modes=floating,click
 
       [mode_floating]
       source=detect
+      label_font_family=Iosevka Term
       label_color=#fb4934ff
       selectable_bg_color=#282828cc
       unselectable_bg_color=#0000
